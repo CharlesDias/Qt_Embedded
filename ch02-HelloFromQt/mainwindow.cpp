257ca9ac967ff2731f1fed9f332d445e6c15ffee
Charles Dias <charlesdias@tecnofuturo.com.br>
2020-07-04 22:41:08 -0300

    Add push button and text line

--- ch02-HelloFromQt/mainwindow.cpp
+++ ch02-HelloFromQt/mainwindow.cpp
@@ -12,6 +12,18 @@ MainWindow::MainWindow(QWidget *parent)
     font = ui->label->font();
     font.setPixelSize(font.pointSize() * ADJUST_PIXEL_SIZE_FOR_STM32MP1);
     ui->label->setFont(font);
+
+    font = ui->label_2->font();
+    font.setPixelSize(font.pointSize() * ADJUST_PIXEL_SIZE_FOR_STM32MP1);
+    ui->label_2->setFont(font);
+
+    font = ui->lineEdit->font();
+    font.setPixelSize(font.pointSize() * ADJUST_PIXEL_SIZE_FOR_STM32MP1);
+    ui->lineEdit->setFont(font);
+
+    font = ui->pushButton->font();
+    font.setPixelSize(font.pointSize() * ADJUST_PIXEL_SIZE_FOR_STM32MP1);
+    ui->pushButton->setFont(font);
 }
 
 MainWindow::~MainWindow()
@@ -19,3 +31,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString name = ui->lineEdit->text();
+    ui->label->setText(QString("Nice to meet you %1!").arg(name));
+}
